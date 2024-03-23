4fc3ac5cffbc67084c12e4b179c230337d5e2e9f
fadlystwn <fadlystwn@gmail.com>
2024-03-23 22:03:43 +0700

    fix style

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -6,8 +6,8 @@ export default async function CarPage() {
   const res = await fetchData(`${process.env.NEXT_PUBLIC_API}/cars`)
   return (
     <main className="container mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold text-center mb-8">Car List</h1>
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+      <h1 className="text-3xl font-bold mb-8">Car List</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
         {
           res.map((item: any) => {
             return (
--- src/app/components/CarList.tsx
+++ src/app/components/CarList.tsx
@@ -5,12 +5,26 @@ import Image from 'next/image'
 export default function CarList({ data }: { data: any }) {
   return (
     <Link href={`/cars/${data.car_id}`}>
-      <div className="bg-white rounded-lg shadow p-4">
-        <Image src="https://placehold.co/600x400/png" width="600" height="400" alt={data.car_id} />
-        <h2 className="text-lg font-semibold mb-2">{data?.model}</h2>
-        <p className="text-slate-500 font-bold text-sm">{data?.type}</p>
-        <p className="text-slate-500 text-xs">{data?.wheels}</p>
+      <div className="bg-white rounded-lg shadow">
+        <Image className="rounded-t-md" src="https://placehold.co/600x400/png" width="600" height="400" alt={data.car_id} />
+        <div className='p-3'>
+          <h2 className="text-slate-800 text-2xl font-semibold mt-2">{data?.model}</h2>
+          <p className="text-slate-500 text-sm mb-2">type: <span className="font-light">{data?.type}</span></p>
+          <p className="text-slate-500 text-xs">{data?.wheels}</p>
+          <p className='text-slate-600 mt-3 text-sm font-bold'>
+            Parts
+          </p>
+          <ul className="flex flex-wrap">
+            {
+              data.parts.map((part: string) => {
+                return (
+                  <li className="border rounded-lg bg-slate-100 text-xs mr-1 my-1 py-1 px-2">{part}</li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
-    </Link>
+    </Link >
   )
 }
