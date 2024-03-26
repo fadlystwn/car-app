c463c938711a9bf3de098b3a0b8496527a75907e
fadlystwn <fadlystwn@gmail.com>
2024-03-26 23:50:46 +0700

    adding headers

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -3,15 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 import cars from '@/data/cars.json';
 
 export async function GET(request: NextRequest) {
+
   const query = request.nextUrl.search.split('?').join('');
   const result = cars.filter(item => item.model.toLowerCase().includes(query?.toLowerCase()));
 
   if (query) {
-    return NextResponse.json(result)
+    return NextResponse.json(result, { headers: { "Content-type": "application/json" } })
 
   } else {
 
-    return NextResponse.json(cars)
+    return NextResponse.json(cars, { headers: { "Content-type": "application/json" } })
   }
 
 }
